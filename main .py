d23ed824706f5279fa4dd16c3770d82144c4fc12
Gustabo845 <agusti.oyarzun@inslapineda.cat>
2024-04-05 19:06:13 +0200

    funcion de cargar los libros

--- main .py
+++ main .py
@@ -16,3 +16,24 @@ def cargar_usuarios():
     except Exception as e:
         print("ERROR: Algo salió mal al cargar usuarios:", e)
     return usuarios
+
+def cargar_libros():
+    libros = []
+    try:
+        file = open("Llibres.txt", "r")
+        for line in file.readlines():
+            datos = line.strip().split("|")
+            libro = {
+                'Título': datos[0],
+                'Autor/a': datos[1],
+                'Año de publicación': datos[2],
+                'Género': datos[3],
+                'ISBN': datos[4]
+            }
+            libros.append(libro)
+        file.close()
+    except FileNotFoundError:
+        print("ERROR: No se encontró el archivo 'Llibres.txt'.")
+    except Exception as e:
+        print("ERROR: Algo salió mal al cargar libros:", e)
+    return libros
